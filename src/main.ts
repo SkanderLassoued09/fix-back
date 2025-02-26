1c04422c6b678a9d20e8f2ce9ecf4bc7018a9ef6
Skander LASSOUED <lassoued.skanderr@gmail.com>
2025-02-26 07:20:53 -0800

    fix ip prod

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '5gb' }));
   app.use(bodyParser.urlencoded({ limit: '5gb', extended: true }));
 
-  await app.listen(3000, () => {
+  await app.listen(3000, '192.168.1.29', () => {
     console.log('running ..');
   });
 }
